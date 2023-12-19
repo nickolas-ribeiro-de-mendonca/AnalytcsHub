d4481a8a34394278390bee9ac35874880344f297
nickolas-ribeiro-de-mendonca <nickolasrm1@hotmail.com>
2023-12-19 06:57:28 -0300

    tabela pronta e botao de select pronto

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,8 +3,7 @@ import {StyleSheet, View, ScrollView} from 'react-native';
 import {Table, Row, Rows} from 'react-native-table-component';
 import commonStyles from '../commonStyles';
 
-
-export default class ExampleTwo extends Component {
+export default class Tables extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
@@ -13,6 +12,11 @@ export default class ExampleTwo extends Component {
 			widthArr: props.widthArr,
 		};
 	}
+   componentDidUpdate(prevProps) {
+      if (prevProps.tableData !== this.props.tableData) {
+        this.setState({ tableData: this.props.tableData });
+      }
+    }
 
 	render() {
 		const state = this.state;
@@ -47,19 +51,20 @@ export default class ExampleTwo extends Component {
 
 const styles = StyleSheet.create({
 	container: {
-		height: 320,
+		maxHeight: 320,
 		padding: 16,
 		paddingTop: 30,
 	},
     table:{
         borderWidth: 0.5,
-        borderColor: commonStyles.colors.cor2
+        borderColor: commonStyles.colors.lightGray
     },
 	head: {
 		height: 30,
 	},
 	text: {
 		textAlign: 'center',
+      color: commonStyles.colors.white
 	},
 	row: {		
 		height: 30,
--- src/screens/SituationApt.js
+++ src/screens/SituationApt.js
@@ -1,60 +1,81 @@
-import React, {useState} from "react";
+import React, {useState} from 'react';
 import {Text, View, StyleSheet, ScrollView} from 'react-native';
-import SelectLists from "../components/SelectList";
-import { tableData, tableHead, widthArr } from "../situacaoData";
-import Tables from "../components/Table";
-import commonStyles from "../commonStyles";
+import SelectLists from '../components/SelectList';
+import {tableData, tableHead, widthArr} from '../situacaoData';
+import Tables from '../components/Table';
+import commonStyles from '../commonStyles';
 
+const dataInitial = tableData.sort((a,b)=>a[9]-b[9])
+const modifiedData = dataInitial.map(item => {
+   const modifiedItem = [...item]; 
+   modifiedItem[1] = item[1].split(' ').slice(0, 3).join(' ');
+   modifiedItem[9] = Number(item[9].toFixed(2));
+   return modifiedItem
+ });
 
 const SituationApt = () => {
-    const [state] = useState({
+	const [state] = useState({
 		tableHead: tableHead,
-		tableData: tableData,
-        widthArr: widthArr
-	})
+		tableData: modifiedData,
+		widthArr: widthArr,
+	});
 
-    const [selectedValue, setSelectedValue] = useState('');
-    //const [filteredData, setFilteredData] = useState(state.tableData)
+	const [selectedValue, setSelectedValue] = useState('');
+	const [filteredData, setFilteredData] = useState(state.tableData);
 
-    const list = () => {
+	const list = () => {
 		const {tableData} = state;
 		const data = [];
-      data.push({key: '', value: 'Empresas'})
+		data.push({key: '', value: 'Empresas'});
 		tableData.forEach(item => {
 			const existingItem = data.find(obj => obj.value === item[2]);
 			if (!existingItem) {
 				data.push({key: item[0], value: item[2]});
 			}
 		});
-        return data;
+		return data;
 	};
 
-    const handleSelect = (value) => {
-        setSelectedValue(value)
-    }
+	const handleSelect = value => {
+		setSelectedValue(value);
+		if (value !== '') {
+			const foundArray = tableData.filter(item => item[0] === value);
+			setFilteredData(foundArray);
 
-    return(
-        <View style={styles.container}>
-            <Text>Texto zuado </Text>
-            <View style={styles.drop}>
-                <SelectLists  data={list} placeholder={'Empresas'} setSelected={handleSelect}/>
-            </View>
-            <View>
-                <Tables tableHead={tableHead} tableData={tableData} widthArr={widthArr}/>
-            </View>
-        </View>
-    )
-}
+		} else {
+			setFilteredData(state.tableData);
+		}
+	};
 
-export default SituationApt
+	return (
+		<View style={styles.container}>
+			<Text>Texto zuado </Text>
+			<View style={styles.drop}>
+				<SelectLists
+					data={list}
+					placeholder={'Empresas'}
+					setSelected={handleSelect}
+				/>
+			</View>
+			<View>
+				<Tables
+					tableHead={state.tableHead}
+					tableData={filteredData}
+					widthArr={state.widthArr}
+				/>
+			</View>
+		</View>
+	);
+};
+
+export default SituationApt;
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor:commonStyles.colors.cor1
-    },
-    drop:{
-        alignItems:"center",
-    },
-    
-})
+	container: {
+		flex: 1,
+		backgroundColor: commonStyles.colors.cor1,
+	},
+	drop: {
+		alignItems: 'center',
+	},
+});
